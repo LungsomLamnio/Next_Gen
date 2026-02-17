c3ce9c3de09ac2d98a7b773e24d4500355c2190f
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2026-02-17 20:11:06 +0530

    favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = {
   title: "Lungsom Lamnio | Next-Gen Portfolio",
   description: "Full Stack Developer & Creative Engineer",
   icons: {
-    icon: "https://res.cloudinary.com/dgmftp80m/image/upload/v1764432799/logo_1.40_Background_Removed.17_AM_lg8mro.png",
+    icon: "https://res.cloudinary.com/dgmftp80m/image/upload/v1771339042/My_Logo_fne0hl.png",
   },
 };
 
